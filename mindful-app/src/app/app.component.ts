a140d01b60c8a58970887993ffd186956d1b476c
Iano-theDev <sinceianmike@gmail.com>
2025-06-18 23:48:01 +0300

    Updated standalone migration, delete unnecessary files

--- mindful-app/src/app/app.component.ts
+++ mindful-app/src/app/app.component.ts
@@ -1,8 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
     selector: 'app-root',
+    standalone: true,
+    imports: [ CommonModule, RouterOutlet],
     templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit {
---
+++ mindful-app/src/app/features/auth/login/login.component.ts
@@ -0,0 +1,64 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/features/auth/auth.service';
+import { LoginRoutingModule } from './login-routing.module';
+import { ButtonModule } from 'primeng/button';
+import { CheckboxModule } from 'primeng/checkbox';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+
+@Component({
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    standalone: true,
+    imports: [
+        CommonModule,
+        LoginRoutingModule,
+        ButtonModule,
+        CheckboxModule,
+        InputTextModule,
+        PasswordModule,
+        ReactiveFormsModule
+    ],
+    styles: [`
+        :host ::ng-deep .pi-eye,
+        :host ::ng-deep .pi-eye-slash {
+            transform:scale(1.6);
+            margin-right: 1rem;
+            color: var(--primary-color) !important;
+        }
+    `]
+})
+export class LoginComponent {
+
+    valCheck: string[] = ['remember'];
+
+    loginForm: FormGroup;
+
+    password!: string;
+    email!: string;
+
+    constructor(private authService: AuthService, private fb: FormBuilder) {
+        // this.loginForm = this.fb.group({
+        //     email: ['', [Validators.required, Validators.email]],
+        //     password: ['', [Validators.required]]
+        // })
+    }
+
+    onSubmit() {
+        console.log("Login Form values: ", this.loginForm.value)
+
+        this.authService.login(this.loginForm.value).subscribe({
+            next: (res) => {
+                console.log("Res is: ", res)
+            },
+            error: err => {
+                console.error("An Error has occured while logging in: ", err)
+            },
+            complete: () => {
+                console.log("We have completed executing the auth service inside the login component ")
+            }
+        })
+    }
+}
