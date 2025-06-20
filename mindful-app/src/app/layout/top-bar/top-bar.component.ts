06c9fcbb642e5dd53fe233bb676e8b6080524c8d
Iano-theDev <sinceianmike@gmail.com>
2025-06-20 18:05:10 +0300

    Update angular version to 18

--- mindful-app/src/app/layout/top-bar/top-bar.component.ts
+++ mindful-app/src/app/layout/top-bar/top-bar.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { ButtonModule } from 'primeng/button';
+import { ToolbarModule } from 'primeng/toolbar';
+
 
 @Component({
   selector: 'app-top-bar',
   standalone: true,
-  imports: [],
+  imports: [ToolbarModule, ButtonModule],
   templateUrl: './top-bar.component.html',
   styleUrl: './top-bar.component.scss'
 })
