c0100aa9762dc955d2616a5171de2e8b81a3bcfa
Iano-theDev <sinceianmike@gmail.com>
2025-06-25 01:34:49 +0300

    Update application to version 20 of angular updated the auth logic in service added a gurad and an interceptor for security

--- mindful-app/src/app/features/auth/auth.service.ts
+++ mindful-app/src/app/features/auth/auth.service.ts
@@ -1,19 +1,26 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private readonly AUTH_TOKEN = 'auth_token'
 
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   login(data: any): Observable<any> {
     console.log("Inside auth service login, data is: ", data)
-    return this.http.post(environment.MINDFUL_SERVER_URL + "/auth/login", data)
+    return this.http.post(environment.MINDFUL_SERVER_URL + "/auth/login", data).pipe(
+      tap(response => {
+        if (response?.token) {
+          localStorage.setItem(this.AUTH_TOKEN, response.token)
+        }
+      })
+    )
   }
 
   signup(data: any): Observable<any> {
@@ -23,6 +30,6 @@ export class AuthService {
 
   logout() {
     localStorage.removeItem('user');
-    // this.router.navigate(['/login']);
+    this.router.navigate(['/login']);
   }
 }
