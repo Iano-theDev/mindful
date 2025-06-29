bedf82ec1dea86de2637ed32af8fac5aa71b5756
Iano-theDev <sinceianmike@gmail.com>
2025-06-29 19:26:11 +0300

    changes to login modularize standalone imports

---
+++ mindful-app/src/app/shared/common.imports.ts
@@ -0,0 +1,7 @@
+import { CommonModule } from '@angular/common';
+import { FormsModule, FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+
+export const COMMON_IMPORTS =  [
+    CommonModule,
+    ReactiveFormsModule
+];
