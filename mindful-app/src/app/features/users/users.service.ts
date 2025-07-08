f52ee18f3a2e2a3c9845a5d79236bb3fec397059
Iano-theDev <sinceianmike@gmail.com>
2025-07-08 23:45:41 +0300

    Add single user profile logic

--- mindful-app/src/app/features/users/users.service.ts
+++ mindful-app/src/app/features/users/users.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { tap } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -13,6 +13,12 @@ export class UsersService {
   getAllUsers() {
     return this.http.get(environment.MINDFUL_SERVER_URL + '/users/').pipe(
       tap( data =>  console.log("Users got in usersService = ", data ))
+      )
+  }
+
+  getSingleUser(id: string) :Observable<any>{
+    return this.http.get(environment.MINDFUL_SERVER_URL + '/users/' + id).pipe(
+      tap( data =>  console.log("Single User in usersService = ", data ))
       )
   }
 }
